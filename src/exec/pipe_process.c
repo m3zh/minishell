cc62a92ef2eb158b775dc16901ebd7265885f5b2
Milena Lazzaretti <mlazzare@student.s19.be>
2021-08-31 16:04:31 +0200

    dup protection

--- src/exec/pipe_process.c
+++ src/exec/pipe_process.c
@@ -6,7 +6,7 @@
 /*   By: mlazzare <mlazzare@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/04 18:17:40 by mlazzare          #+#    #+#             */
-/*   Updated: 2021/08/31 14:01:41 by mlazzare         ###   ########.fr       */
+/*   Updated: 2021/08/31 16:02:44 by mlazzare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,40 +23,56 @@ static void parent_waits(t_shell s, pid_t proc)
 static void open_fd(t_shell *s)
 {
 	s->file.tmpin = dup(0);
+	if (s->file.tmpin < 0)
+		ft_exit(s);
 	s->file.tmpout = dup(1);
+	if (s->file.tmpout < 0)
+		ft_exit(s);
 	if (s->file.infile)
 		redir_input(s);
 	else
+	{
 		s->file.fdin = dup(s->file.tmpin);
+		if (s->file.fdin < 0)
+			ft_exit(s);
+	}
 	s->file.fdout = dup(s->file.tmpout);
+	if (s->file.fdout < 0)
+		ft_exit(s);
 }
 
 static void swap_pipe(t_shell *s, int i)
 {
 	if (s->file.infile)
 		redir_input(s);
-	dup2(s->file.fdin, 0);
+	dup2(s->file.fdin, 0); // protecting this creates troubles, to check
 	close(s->file.fdin);
 	if (i == s->len - 1)
 	{
 		if (s->file.outfile)
 			redir_output(s);
 		else
+		{
 			s->file.fdout = dup(s->file.tmpout);
+			if (!s->file.fdout)
+				ft_exit(s);
+		}
 	}		
 	else
 	{
 		s->file.fdout = s->pipefd[1];
 		s->file.fdin = s->pipefd[0];
 	}
-	dup2(s->file.fdout, 1);
+	dup2(s->file.fdout, 1); // protecting this creates troubles, to check
 	close(s->file.fdout);
 }
 
 static void close_fd(t_shell *s)
 {
-	dup2(s->file.tmpin, 0);
-	dup2(s->file.tmpout, 1);
+	if (dup2(s->file.tmpin, 0) < 0)
+		ft_exit(s);
+	if (dup2(s->file.tmpout, 1)  < 0)
+		ft_exit(s);
 	close(s->file.tmpin);
 	close(s->file.tmpout);
 	close(s->pipefd[0]);
