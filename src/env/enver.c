25084f41513ecc2938cf10f5f0148e6e1e6f2e8f
maxdesalle <maxdesalle@pm.me>
2021-08-31 11:46:17 +0200

    added unset NAMEOFVAR

--- src/env/enver.c
+++ src/env/enver.c
@@ -6,7 +6,7 @@
 /*   By: maxdesalle <mdesalle@student.s19.be>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 10:13:13 by maxdesall         #+#    #+#             */
-/*   Updated: 2021/08/31 11:13:40 by maxdesall        ###   ########.fr       */
+/*   Updated: 2021/08/31 11:46:01 by maxdesall        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,18 @@
 
 void	enver(t_shell *shell)
 {
+	char	**tab;
+
 	if (comp("$", shell->cmd[0]))
 	{
 		free(shell->cmd[0]);
 		shell->cmd[0] = get_var(shell, ft_substr(shell->cmd[0], 1, ft_strlen(shell->cmd[0])));
+	}
+	else if (comp("unset ", shell->cmd[0]))
+	{
+		tab = ft_split(shell->cmd[0], ' ');
+		if (unsetenv(tab[1]) == -1)
+			perror("unset command failed");
+		free(tab);
 	}
 }
