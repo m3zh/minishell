f9c5dd9d654e7507afb990d7b41edb6523287a2b
maxdesalle <maxdesalle@pm.me>
2021-08-31 18:25:46 +0200

    added export function

--- src/env/enver.c
+++ src/env/enver.c
@@ -6,11 +6,38 @@
 /*   By: maxdesalle <mdesalle@student.s19.be>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 10:13:13 by maxdesall         #+#    #+#             */
-/*   Updated: 2021/08/31 11:46:01 by maxdesall        ###   ########.fr       */
+/*   Updated: 2021/08/31 17:23:41 by maxdesall        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
+
+static void	exporter(char *str)
+{
+	int		i;
+	int		tmp;
+	char	*var;
+	char	*value;
+
+	i = 0;
+	while (str[i] && str[i] != '=')
+		i += 1;
+	if (str[i] == '=')
+	{
+		tmp = i;
+		i -= 1;
+		while (str[i] >= 65 && str[i] <= 90 && i > 0)
+			i -= 1;
+		var = ft_substr(str, i, tmp);
+		i = tmp + 1;
+		while (str[i] && str[i] != ' ' && (!(str[i] >= 9 && str[i] <= 13)))
+			i += 1;
+		value = ft_substr(str, tmp + 1, i);
+		if (setenv(var, value, 1) == -1)
+			perror("setenv function failed");
+	}
+	free(str);
+}
 
 void	enver(t_shell *shell)
 {
@@ -27,5 +54,9 @@ void	enver(t_shell *shell)
 		if (unsetenv(tab[1]) == -1)
 			perror("unset command failed");
 		free(tab);
+	}
+	else if (comp("export ", shell->cmd[0]))
+	{
+		exporter(ft_substr(shell->cmd[0], comp("export", shell->cmd[0]), ft_strlen(shell->cmd[0])));
 	}
 }
