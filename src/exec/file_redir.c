c48da7c3dbdb1f5cba055a211c5f51fe7af28d56
mlazzare <mlazzare@student.s19.be>
2021-09-07 13:53:27 +0200

    echo | cat

--- src/exec/file_redir.c
+++ src/exec/file_redir.c
@@ -6,7 +6,7 @@
 /*   By: mlazzare <mlazzare@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/14 15:17:14 by mlazzare          #+#    #+#             */
-/*   Updated: 2021/09/07 13:37:08 by mlazzare         ###   ########.fr       */
+/*   Updated: 2021/09/07 13:53:07 by mlazzare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void    redir_output(t_shell *s)
 {
-    printf("here");
     if (s->file.ow)
         s->file.fdout = open(s->file.outfile, O_CREAT | O_RDWR | O_TRUNC, 0644);
     else if (s->file.ap)
