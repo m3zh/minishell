10e51677e21baf5afa20e222970af3b8f17a2b80
mlazzare <mlazzare@student.s19.be>
2021-09-07 09:49:57 +0200

    echo fix

--- src/parser/check_quotes.c
+++ src/parser/check_quotes.c
@@ -6,11 +6,16 @@
 /*   By: mlazzare <mlazzare@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/13 10:31:59 by mlazzare          #+#    #+#             */
-/*   Updated: 2021/09/06 12:49:03 by mlazzare         ###   ########.fr       */
+/*   Updated: 2021/09/07 09:47:51 by mlazzare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
+
+static int  is_specialchar(char c)
+{
+    return (c == SINGLEQTS || c == DOLLARSIGN);
+}
 
 static char *dollar2value(char *s, int start, int size)
 {
@@ -40,44 +45,24 @@ static void doubleqts_stringify(t_shell *s, char **arg, int i)
 {
     int     j;
     int     k;
-    int     start;
     char    *tmp;
-    char    *var;
 
-    j = -1;
-    k = 0;
-    tmp = malloc(sizeof(char) * MAX + 1); // to check for leaks ?
-    if (!tmp)
-        malloxit();
-    while (arg[i][++j])  // doublequotes ascii code is 34
+    while (arg[i])  // singlequotes ascii code is 39
     {
-        if (arg[i][j] == DOLLARSIGN && (j == 0 || arg[i][j - 1] != BACKSLASH))
-        {
-            j++;
-            start = j;
-            while (arg[i][j]
-                && !ft_space(arg[i][j]) && arg[i][j] != DOUBLEQTS)
-                j++;
-            var = dollar2value(arg[i], start, j - start);
-            start = 0;
-            while (tmp[k] && var[start])
-                tmp[k++] = var[start++];
-            free(var);
-            j--;
-        }
-        else if (arg[i][j] != DOUBLEQTS ||
-                (j > 0 && arg[i][j - 1] == BACKSLASH
-                && arg[i][j] == DOUBLEQTS))
-            tmp[k++] = arg[i][j];
+        k = 0;
+        j = -1;
+        tmp = malloc(sizeof(char) * MAX + 1); // to check for leaks ?
+        if (!tmp)
+            malloxit();
+        while (arg[i][++j])
+            if ((arg[i][j] != DOUBLEQTS
+                && !(arg[i][j] == BACKSLASH && arg[i][j + 1] && is_specialchar(arg[i][j + 1]))) // horrible, to refactor in a function is_specialchar
+                || (j > 0 && arg[i][j] == DOUBLEQTS && arg[i][j - 1] == BACKSLASH))
+                tmp[k++] = arg[i][j];
+        tmp[k] = 0;
+        str_replace(&arg[i], tmp);
+        i++;
     }
-    tmp[k] = 0;
-    str_replace(&arg[i], tmp);
-    // if (arg[i])
-    //     free(arg[i]);
-    // arg[i] = ft_strdup(tmp);
-    // if (!arg[i])
-    //     malloxit();
-    // free(tmp);  
     s->var.double_qts = 0;
 }
 
@@ -85,30 +70,26 @@ static void doubleqts_stringify(t_shell *s, char **arg, int i)
 *  This function removes single quotes from the string
 */
 
-static void singleqts_stringify(t_shell *s, char **arg, int i)
+static void singleqts_stringify(char **arg, int i)
 {
     int     j;
     int     k;
     char    *tmp;
-
-    j = -1;
-    k = 0;
-    tmp = malloc(sizeof(char) * ft_strlen(arg[i]) + 1); // to check for leaks ?
-    if (!tmp)
-        malloxit();
-    while (arg[i][++j])  // singlequotes ascii code is 39
+        
+    while (arg[i])  // singlequotes ascii code is 39
     {
-        if (arg[i][j] != SINGLEQTS)
-            tmp[k++] = arg[i][j];
+        k = 0;
+        j = -1;
+        tmp = malloc(sizeof(char) * MAX + 1); // to check for leaks ?
+        if (!tmp)
+            malloxit();
+        while (arg[i][++j])
+            if (arg[i][j] != SINGLEQTS)
+                tmp[k++] = arg[i][j];
+        tmp[k] = 0;
+        str_replace(&arg[i], tmp);
+        i++;
     }
-    tmp[k] = 0;
-    str_replace(&arg[i], tmp);
-    // free(arg[i]);
-    // arg[i] = ft_strdup(tmp);
-    // if (!arg[i])
-    //     malloxit();
-    // free(tmp);
-    (void)s;
 }
 
 void check_quotes(t_shell *s, char **arg, int i)
@@ -118,7 +99,7 @@ void check_quotes(t_shell *s, char **arg, int i)
     else if (starts_with("\"", arg[i]))
         s->var.double_qts = 1;       
     if (s->var.single_qts)
-        singleqts_stringify(s, arg, i);
+        singleqts_stringify(arg, i);
     else if (s->var.double_qts)
         doubleqts_stringify(s, arg, i);
 }
@@ -129,7 +110,8 @@ void check_echo(t_shell *s, char **arg, int i)
 
     if (s->var.single_qts || s->var.double_qts)
         return ;
-    if (!ft_strcmp(arg[0], "echo") && starts_with("$", arg[i]))
+    if (!ft_strcmp(arg[0], "echo") && starts_with("$", arg[i])
+        && ft_strcmp("$", arg[i]))
     {
         tmp = dollar2value(arg[i], 1, sizeof(arg[i]));
         if (!tmp)
