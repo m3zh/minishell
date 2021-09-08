8adbc79894a31f5ff8a08c5bc7faed02a572260c
mlazzare <mlazzare@student.s19.be>
2021-09-08 15:05:06 +0200

    exit msg

--- src/exec/exit.c
+++ src/exec/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maxdesalle <mdesalle@student.s19.be>       +#+  +:+       +#+        */
+/*   By: mlazzare <mlazzare@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/06 10:56:49 by maxdesall         #+#    #+#             */
-/*   Updated: 2021/09/06 15:40:52 by maxdesall        ###   ########.fr       */
+/*   Updated: 2021/09/08 15:04:06 by mlazzare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,8 @@
 void	stop(t_shell *shell)
 {
 	if (starts_with("exit", shell->cmd[0]) && ft_strlen(shell->cmd[0]) == 4)
+	{
+		printf("exit\n");
 		exit(EXIT_SUCCESS);
+	}		
 }
