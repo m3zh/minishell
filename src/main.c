2aff18b962af9a586a658847fdb7cb3171253bf6
maxdesalle <maxdesalle@pm.me>
2021-09-08 16:50:39 +0200

    quick fix?

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mlazzare <mlazzare@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/06 15:43:12 by maxdesall         #+#    #+#             */
-/*   Updated: 2021/09/08 16:47:45 by maxdesall        ###   ########.fr       */
+/*   Updated: 2021/09/08 16:50:32 by maxdesall        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,16 @@ static int	checker(t_shell *shell)
 {
 	int	i;
 
- 	i = 0;
- 	if (!shell->cmd[0])
- 		return (0);
- 	while (shell->cmd[0][i]
+	i = 0;
+	if (!shell->cmd[0])
+		return (0);
+	while (shell->cmd[0][i]
 			&& ((shell->cmd[0][i] >= 9
 					&& shell->cmd[0][i] <= 13) || shell->cmd[0][i] == ' '))
- 		i += 1;
- 	if (shell->cmd[0][i] == '\0')
- 		return (0);
- 	return (1);
+		i += 1;
+	if (shell->cmd[0][i] == '\0')
+		return (0);
+	return (1);
 }
 
 int	ft_history(char *str, char *cmd)
@@ -45,17 +45,17 @@ static void	prompt(t_shell *shell, char **envp)
 	char	*input;
 	char	str[MAX];
 
-    while (BASH)
-    {
+	while (BASH)
+	{
 		input = readline("minishell$ ");
 		if (!input)
 			handle_sigusr1(SIGUSR1);
 		ft_history(str, input);
-        parse_shell(shell, input, envp);
+		parse_shell(shell, input, envp);
 		if (checker(shell))
 			exec_shell(*shell);
 		// system("leaks minishell");
-    }
+	}
 }
 
 int main(int ac, char **ag, char **envp)
@@ -63,11 +63,11 @@ int main(int ac, char **ag, char **envp)
 	t_shell	shell;
 
 	if (ac > 1)
-        return (printf("Error\nInvalid number of arguments\n"));
-    (void)ag;
+		return (printf("Error\nInvalid number of arguments\n"));
+	(void)ag;
 	signal(SIGINT, handle_sigint);
 	signal(SIGUSR1, handle_sigusr1);
 	signal(SIGQUIT, handle_sigquit);
 	prompt(&shell, envp);
-    return (0);
+	return (0);
 }
