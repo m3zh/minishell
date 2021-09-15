d80f17d64c374068ac72fa336f7912be0ada3eec
mlazzare <mlazzare@student.s19.be>
2021-09-15 14:51:45 +0200

    export invalid identifier

--- src/env/expoorter.c
+++ src/env/expoorter.c
@@ -6,13 +6,48 @@
 /*   By: mlazzare <mlazzare@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 12:20:20 by mdesalle          #+#    #+#             */
-/*   Updated: 2021/09/15 14:12:55 by mlazzare         ###   ########.fr       */
+/*   Updated: 2021/09/15 14:49:59 by mlazzare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
 /* assistant to the exporter */
+
+static int	valid_export(char *str, int i)
+{
+	char *s;
+	int start;
+
+	if (!ft_isalpha(str[i]))
+	{
+		start = i;
+		while (str[i] && (!ft_space(str[i]) && str[i] != '='))
+			i++;
+		s = ft_substr(str, start, i - start);
+		if (!s)
+			malloxit();
+		printf("bash: export: \\`%s\': not a valid identifier\n", s);
+		return (0);		
+	}
+	return (1);
+}
+
+static void	check_nextExport(t_shell *shell, int l)
+{
+	char	*s;
+
+	while (shell->cmd[0][l] && ft_space(shell->cmd[0][l]))
+		l++;
+	if (shell->cmd[0][l] && shell->cmd[0][l + 7])
+	{
+		s = ft_substr(shell->cmd[0], l, 7);
+		if (!s)
+			malloxit();
+		if (!ft_strcmp(s, "export "))
+			exporter(shell, l + 7);
+	}
+}
 
 static void	assistant(t_shell *shell, char *str, char *var, int i)
 {
@@ -47,7 +82,6 @@ void	exporter(t_shell *shell, int l)
 	int		start;
 	char	*str;
 	char	*var;
-	char	*s;
 
 	while (shell->cmd[0][l] && ft_space(shell->cmd[0][l]))
 		l += 1;
@@ -58,28 +92,19 @@ void	exporter(t_shell *shell, int l)
 	if (!str)
 		malloxit();
 	i = 0;
+	if (!valid_export(str, i))
+		check_nextExport(shell, l);
 	while (str[i] && str[i] != '=')
 		i += 1;
-	if (!str[i] || str[i] != '=')
-	{
-		free(str);
+	if (!str[i])
 		return ;
-	}
+	if (str[i] != '=')
+		check_nextExport(shell, l);
 	var = ft_substr(str, 0, i);
 	if (!var)
 		malloxit();
 	assistant(shell, str, var, i);
 	free(var);
 	free(str);
-	while (shell->cmd[0][l] && ft_space(shell->cmd[0][l]))
-		l++;
-	if (shell->cmd[0][l] && shell->cmd[0][l + 7])
-	{
-		s = ft_substr(shell->cmd[0], l, 7);
-		if (!s)
-			malloxit();
-		if (!ft_strcmp(s, "export "))
-			exporter(shell, l + 7);
-		free(s);
-	}
+	check_nextExport(shell, l);
 }
